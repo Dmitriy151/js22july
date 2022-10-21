8c668f986e67ec09e5f43cebbf4d64a5c2fa3de2
Dmitriy151 <nova151kra@gmail.com>
2022-10-21 16:49:05 +0600

    21.10

---
+++ telegramm1/tg.js
@@ -0,0 +1,12 @@
+/*let tag;
+tag = document.getElementById("out")
+
+
+let result = '';
+for (let i = 4; i <= 400; i++) {
+   result = result + i + " ";
+}
+//result = 5;
+tag.innerHTML = result;
+console.log(result);
+alert('hello')*/
